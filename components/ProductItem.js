43f104d7a0521fca97480d774c5666301ddfdbd2
Nguyen Duc Nhat <msv14106173@gmail.com>
2020-09-03 20:24:33 +0700

    1

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,21 +1,47 @@
 import React from 'react'
-import {StyleSheet, View, Text, Image, TouchableOpacity, Alert, Dimensions} from 'react-native'
-import {numberFormat} from '../js/main';
+import { StyleSheet, View, Text, Image, TouchableOpacity, Alert, Dimensions } from 'react-native'
+import { numberFormat } from '../js/main';
 import Style from "../js/Style";
 import Constants from '../Constants';
+import Color from './Color';
 
 export default function ProductItem(props) {
     const { product, onPress, style } = props;
+    let discount = 0;
+    if (product.price && product.price > product.sale_price) {
+        discount = 100 - Math.round(product.sale_price / product.price * 100 );
+    }
     return (
-        <TouchableOpacity onPress={onPress} style={{flex: 1}}>
+        <TouchableOpacity onPress={onPress} style={{ flex: 1 }}>
             <View style={[Style.card, styles.productItem, style]}>
-                <Image
-                    style={ styles.img }
-                    source={{uri: Constants.base_url + product.image}}
-                />
-                <View style={{padding: 10}}>
-                    <Text numberOfLines={2} style={ styles.title }>{ product.name.toUpperCase() }</Text>
-                    <Text style={styles.price}>{numberFormat(product.sale_price * Constants.token_rate) + ' đ'}</Text>
+                <View style={{ position: 'relative', width: '100%', overflow: 'hidden', borderTopRightRadius: 5, borderTopLeftRadius: 5 }}>
+                    <Image
+                        style={styles.img}
+                        source={{ uri: Constants.base_url + product.image }}
+                    />
+                    {discount > 0 
+                    ? <View style={styles.labelTopRight}>
+                        <Text style={styles.labelText}>{discount}%</Text>
+                        <Text style={styles.labelText}>Giảm</Text>
+                        <View style={styles.labelTopRightBefore}></View>
+                    </View>
+                    : <View></View>
+                }
+                </View>
+                <View style={styles.labelTopLeft}>
+                    <Text style={styles.labelText}>VNS MALL</Text>
+                    <View style={styles.labelTopLeftBefore}></View>
+                </View>
+                
+                <View style={{ padding: 10, width: '100%' }}>
+                    <Text numberOfLines={2} style={styles.title}>{product.name.toUpperCase()}</Text>
+                    <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
+                        <Text style={styles.price}>{numberFormat(product.sale_price * Constants.token_rate) + 'đ'}</Text>
+                        {discount > 0
+                            ? <Text style={styles.rootPrice}>{numberFormat(product.price * Constants.token_rate) + 'đ'}</Text>
+                            : <Text></Text>
+                        }
+                    </View>
                 </View>
             </View>
         </TouchableOpacity>
@@ -24,25 +50,79 @@ export default function ProductItem(props) {
 
 const styles = StyleSheet.create({
     productItem: {
-        alignItems: 'center', 
-        margin: 5, 
-        padding: 0, 
-        width: (Dimensions.get('window').width - 30) / 2
-    },  
+        alignItems: 'center',
+        margin: 5,
+        padding: 0,
+        width: (Dimensions.get('window').width - 30) / 2,
+        overflow: 'visible',
+    },
     title: {
+        fontSize: 12,
         textAlign: 'center',
-        lineHeight: 16,
-        height: 32,
+        lineHeight: 12,
+        height: 24,
         marginBottom: 5
     },
     img: {
         height: (Dimensions.get('window').width - 100) / 2,
         width: '100%',
-        marginHorizontal: 'auto'
+        marginHorizontal: 'auto',
     },
     price: {
         color: 'tomato',
-        fontSize: 18,
+        fontSize: 14,
+        lineHeight: 14,
+        marginRight: 5,
+    },
+    rootPrice: {
+        color: Color.muted,
+        fontSize: 9,
+        textDecorationLine: 'line-through'
+    },
+    labelTopLeft: {
+        backgroundColor: '#269ED2',
+        position: 'absolute',
+        paddingHorizontal: 3,
+        borderRadius: 3,
+        borderBottomLeftRadius: 0,
+        top: 5,
+        left: -3
+    },
+    labelTopLeftBefore: {
+        position: 'absolute',
+        left: 0,
+        bottom: -3,
+        borderLeftWidth: 4,
+        borderLeftColor: 'transparent',
+        borderTopColor: '#0170BF',
+        borderTopWidth: 3
+    },
+    labelTopRight: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        backgroundColor: 'tomato',
+        width: 30,
+        paddingTop: 0,
+        paddingBottom: 5,
+        borderTopRightRadius: 5, 
+    },
+    labelTopRightBefore: {
+        width: 30,
+        height: 6,
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        bottom: -6,
+        borderBottomColor: 'transparent',
+        borderBottomWidth: 6,
+        borderLeftColor: 'tomato',
+        borderLeftWidth: 15,
+        borderRightColor: 'tomato',
+        borderRightWidth: 15
+    },
+    labelText: {
+        color: 'white',
+        fontSize: 9,
         textAlign: 'center'
     }
 });
--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {SafeAreaView, View, Text, ActivityIndicator, ScrollView, Image, Dimensions, StyleSheet, RefreshControl, TouchableOpacity, FlatList} from 'react-native';
-import {Title, Avatar, Button, Caption} from 'react-native-paper';
+import { SafeAreaView, View, Text, ActivityIndicator, ScrollView, Image, Dimensions, StyleSheet, RefreshControl, TouchableOpacity, FlatList } from 'react-native';
+import { Title, Avatar, Button, Caption } from 'react-native-paper';
 import Axios from 'axios'
 import Toast from "react-native-tiny-toast";
 
@@ -8,6 +8,7 @@ import Style from "../js/Style";
 import Constants from "../Constants";
 import Color from '../components/Color';
 import ProductItem from '../components/ProductItem';
+import { preventAutoHide } from 'expo/build/launch/SplashScreen';
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -15,73 +16,75 @@ export default class Home extends React.Component {
         this.state = ({
             banners: null,
             categories: [],
-            categoryProducts: [],
+            highlights: [],
+            recommends: [],
             refreshing: false,
             readyData: false
         });
     }
 
     componentDidMount() {
-        this._loadData(); 
+        this._loadData();
     }
 
     _loadData = (refresh = false) => {
         let loading = null;
         if (refresh) {
-            loading = Toast.show('Đang làm mới...', {duration: 5000});
+            loading = Toast.show('Đang làm mới...', { duration: 5000 });
         }
         Axios.get('/get-home')
             .then(res => {
                 this.setState({
                     banners: res.data.banners,
                     categories: res.data.categories,
-                    categoryProducts: res.data.categoryProducts,
+                    highlights: res.data.highlights,
+                    recommends: res.data.recommends,
                     readyData: true,
                     refreshing: false,
                 });
                 if (refresh) {
                     Toast.hide(loading);
-                    Toast.show('Đã làm mới',  {duration: 500})
+                    Toast.show('Đã làm mới', { duration: 500 })
                 }
             })
             .catch(error => console.warn(error));
     }
 
     onRefresh = () => {
-        this.setState({refreshing: true});
+        this.setState({ refreshing: true });
         this._loadData(true);
     }
 
     renderCategories = () => {
-        const {categories} = this.state;
-        const {navigation} = this.props;
+        const { categories } = this.state;
+        const { navigation } = this.props;
 
         return (
-            <View style={{marginHorizontal: 7, marginBottom: 30}}>
+            <View style={{ marginHorizontal: 7, marginBottom: 30 }}>
                 <ScrollView horizontal={true}>
                     {categories.map((item, index) => {
-                        if (index % 2 == 0){ 
+                        if (index % 2 == 0) {
                             return (
                                 <View key={index}>
                                     <TouchableOpacity style={[Style.card, styles.categoryItem]} onPress={() => navigation.navigate('Products', {
                                         category: item
                                     })}>
-                                        <View style={{padding: 5, borderRadius: 50, backgroundColor: Color.alert_info}}>
-                                            <Image style={{width: 40, height: 40}} source={{uri: Constants.base_url + item.avatar}} />
+                                        <View style={{ padding: 5, borderRadius: 50, backgroundColor: Color.alert_info }}>
+                                            <Image style={{ width: 40, height: 40 }} source={{ uri: Constants.base_url + item.avatar }} />
                                         </View>
-                                        <Text numberOfLines={2} style={{fontSize: 10, textAlign: 'center'}}>{item.name}</Text>
+                                        <Text numberOfLines={2} style={{ fontSize: 10, textAlign: 'center' }}>{item.name}</Text>
                                     </TouchableOpacity>
-                                    
+
                                     {typeof categories[index + 1] != "undefined"
                                         ? <TouchableOpacity style={[Style.card, styles.categoryItem]} onPress={() => navigation.navigate('Products', {
-                                                category: categories[index + 1]
-                                            })}>
-                                            <View style={{padding: 5, borderRadius: 50, backgroundColor: Color.alert_info}}>
-                                                <Image style={{width: 40, height: 40}} source={{uri: Constants.base_url + categories[index + 1].avatar}} />
+                                            category: categories[index + 1]
+                                        })}>
+                                            <View style={{ padding: 5, borderRadius: 50, backgroundColor: Color.alert_info }}>
+                                                <Image style={{ width: 40, height: 40 }} source={{ uri: Constants.base_url + categories[index + 1].avatar }} />
                                             </View>
-                                            <Text numberOfLines={2} style={{fontSize: 10, textAlign: 'center'}}>{categories[index + 1].name}</Text>
+                                            <Text numberOfLines={2} style={{ fontSize: 10, textAlign: 'center' }}>{categories[index + 1].name}</Text>
                                         </TouchableOpacity>
-                                        : <View style={{display: 'none'}}></View>
+                                        : <View style={{ display: 'none' }}></View>
                                     }
                                 </View>
                             );
@@ -92,46 +95,66 @@ export default class Home extends React.Component {
         );
     }
 
-    renderProducts () {
-        const {categoryProducts} = this.state;
+    renderHighLights() {
+        const {highlights} = this.state;
         const {navigation} = this.props;
-        
 
+        if (!highlights.length) return;
+        
+        return (
+            <View style={{ marginBottom: 30 }}>
+                <View style={{ position: 'relative' }}>
+                    <Text style={styles.productsGroup}>Sản phẩm nổi bật</Text>
+                    <View style={ styles.division }></View>
+                </View>
+                <FlatList
+                    data={highlights}
+                
+                    renderItem={({ item }) => <ProductItem style={styles.productItem} product={item} onPress={() => navigation.navigate('ProductDetail', {
+                        productId: item.id
+                    })} />}
+                    keyExtractor={(item) => `${item.id}`}
+                    ListEmptyComponent={() => <Text style={Style.noData}>Không có dữ liệu</Text>}
+                    horizontal={true}
+                />
+            </View>
+        );
+    }
+
+    renderRecommends() {
+        const {recommends} = this.state;
+        const {navigation} = this.props;
+
+        if (!recommends.length) return;
+        
         return (
             <View>
-                {categoryProducts.map((item, index) => {
-                    let products = item.products;
-                    return (
-                        <View style={{marginBottom: 30, marginHorizontal: 10}} key={index}>
-                            <TouchableOpacity style={{backgroundColor: Color.secondary, width: (Dimensions.get('window').width / 1.8), padding: 10, borderRadius: 5, marginBottom: 5}} onPress={() => navigation.navigate('Products', {
-                                category: item
-                            })}>
-                                <Text numberOfLines={1} style={{color: '#fff'}}>{item.name.toUpperCase()}</Text>
-                            </TouchableOpacity>
-                            <FlatList
-                                data={products}
-                                renderItem={({ item }) => <ProductItem style={styles.productItem} product={ item } onPress={() => navigation.navigate('ProductDetail', {
-                                    productId: item.id
-                                })} />}
-                                keyExtractor={(item) => `${item.id}`}
-                                ListEmptyComponent={() => <Text style={Style.noData}>Không có dữ liệu</Text>}
-                                horizontal={true}
-                            />
-                        </View>
-                    );
-                })}
+                <View style={{ position: 'relative' }}>
+                    <Text style={[styles.productsGroup]}>Gợi ý cho bạn</Text>
+                    <View style={ styles.division }></View>
+                </View>
+                <FlatList
+                    data={recommends}
+                    style={{ padding: 5 }}
+                    renderItem={({ item }) => <ProductItem product={item} onPress={() => navigation.navigate('ProductDetail', {
+                        productId: item.id
+                    })} />}
+                    keyExtractor={(item) => `${item.id}`}
+                    ListEmptyComponent={() => <Text style={Style.noData}>Không có dữ liệu</Text>}
+                    numColumns={2}
+                />
             </View>
         );
     }
 
     render() {
-        const {banners, categoryProducts, readyData, refreshing} = this.state;
+        const { banners, categoryProducts, readyData, refreshing } = this.state;
 
         if (!readyData) {
-            return <View style={{flex: 1, justifyContent: 'center'}}>
+            return <View style={{ flex: 1, justifyContent: 'center' }}>
                 <ActivityIndicator size="large" />
             </View>
-        }else{
+        } else {
             return (
                 <ScrollView
                     refreshControl={
@@ -139,14 +162,16 @@ export default class Home extends React.Component {
                     }
                 >
                     <SafeAreaView style={Style.container}>
-                        <Image source={{uri: Constants.base_url + banners.image}} style={{width: Dimensions.get('window').width, height: 120, marginBottom: 30}} />
+                        <Image source={{ uri: Constants.base_url + banners.image }} style={{ width: Dimensions.get('window').width, height: 120, marginBottom: 30 }} />
 
                         {this.renderCategories()}
-                        
-                        {this.renderProducts()}
-                        
-                        <Caption style={{textAlign: 'center', marginVertical: 15}}>{Constants.sologan}</Caption>
-                        
+
+                        {this.renderHighLights()}
+
+                        {this.renderRecommends()}
+
+                        <Caption style={{ textAlign: 'center', marginVertical: 15 }}>{Constants.sologan}</Caption>
+
                     </SafeAreaView>
                 </ScrollView>
             );
@@ -157,8 +182,8 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
     categoryItem: {
         alignItems: 'center',
-        height: 90, 
-        width: 90, 
+        height: 90,
+        width: 90,
         justifyContent: 'space-between',
         margin: 3,
         shadowRadius: 0,
@@ -168,5 +193,19 @@ const styles = StyleSheet.create({
         margin: 0,
         marginRight: 10,
         width: (Dimensions.get('window').width - 30) / 2
+    },
+    productsGroup: {
+        textAlign: 'center',
+        marginBottom: 10,
+        color: Color.secondary,
+    },
+    division: {
+        backgroundColor: Color.secondary,
+        height: 1,
+        width: 50,
+        position: 'absolute',
+        left: '50%',
+        bottom: 10,
+        transform: [{ translateX: -25 }]
     }
 });
